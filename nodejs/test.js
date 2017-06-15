cbca68db001b5328efdbaff71d7fe259eb33acbc
opnegion <opnegion>
2017-06-15 17:47:00 +0900

    0615

--- nodejs/test.js
+++ nodejs/test.js
@@ -1,3 +1,9 @@
+class Test{
+    constructor(val1){
+        this.val1 = val1;
+    }
+}
+
 function main(){
     console.log('hello world');
 }
--- sonarqube/setpath.bat
+++ sonarqube/setpath.bat
@@ -1,4 +1,4 @@
-set path=%path%;C:\Program Files\sonar-scanner-3.0.3.778-windows\bin;
+set path=%path%;H:\opengion_user\programFiles\sonar-scanner-3.0.3.778-windows\bin;
 
 rem cd /d H:\opengion_user\workVSCode\jstest\javascript
 rem sonar-scanner.bat
