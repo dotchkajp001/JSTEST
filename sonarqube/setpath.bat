9cbdc1b7da7843dc535e8a8b88bdc677a5836e6d
opnegion <opnegion>
2017-06-23 17:38:46 +0900

    0623

--- sonarqube/setpath.bat
+++ sonarqube/setpath.bat
@@ -2,3 +2,7 @@ set path=%path%;H:\opengion_user\programFiles\sonar-scanner-3.0.3.778-windows\bi
 
 rem cd /d H:\opengion_user\workVSCode\jstest\javascript
 rem sonar-scanner.bat
+
+rem cd /d H:\opengion_user\workOpengion\download\openGion_dev_5.9.21.0\sonartest
+cd /d Z:\jk
+
